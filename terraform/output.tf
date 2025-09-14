cdd14c9d6dd8e2847eb8e14d44269cbe51382fc8
Joey Hofman <joeyhofman1@gmail.com>
2025-09-14 15:37:49 +0200

    opdracht 2

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,5 +7,8 @@ resource "local_file" "ansible_inventory_file" {
   content = <<EOT
 [all]
 ${esxi_guest.ubuntu_test.ip_address} app_name=demoapp
+[all:vars]
+ansible_user=terraform
+ansible_ssh_private_key_file=${var.ssh_public_key_path}
 EOT
 }
